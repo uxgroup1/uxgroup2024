2ecbc954e525b8203b241688a99f7f5d2928f64e
Kauê Lima <kaue.slima000@gmail.com>
2024-02-29 12:51:31 -0300

    Ajuste de telas

--- src/components/logisticaComPerformance/index.tsx
+++ src/components/logisticaComPerformance/index.tsx
@@ -7,43 +7,43 @@ const content = [
     title: "Armazenagem e expedição de pedidos ",
     text: "Todos os produtos armazenados e expedidos com 99,6% do cumprimento de SLA para uma operação eficiente com foco na experiência do cliente.",
     banner: "",
-    index : 0
+    index: 0,
   },
   {
     title: "Gestão de estoque",
     text: "Reduza custos com organização, rastreabilidade e controle dos produtos em estoque e das suas vendas, garantindo eficiência operacional e satisfação do cliente.",
     banner: "",
-    index : 1
+    index: 1,
   },
   {
     title: "Logística Reversa",
     text: "Transforme a devolução de produtos em oportunidades de satisfação. Simplifique as etapas com a garantia do mesmo cuidado com a expedição.",
     banner: "",
-    index : 2
+    index: 2,
   },
   {
     title: "Gestão de transportes",
     text: "Otimize o seu transporte, com uma gestão de entregas 100% integradas a tecnologia permitindo tomada de decisão rápida e assertiva, garantindo o cuidado com a jornada do pedido.",
     banner: "",
-    index : 3
+    index: 3,
   },
   {
     title: "PUDO's",
     text: "Aumente a satisfação do cliente com nossos pontos de coleta e entrega estrategicamente localizados. A conveniência e a agilidade ao alcance do seu cliente.",
     banner: "",
-    index : 4
+    index: 4,
   },
   {
     title: "WMS",
     text: "Automação total para uma operação de e-commerce de alta performance. Digitalização e visibilidade em tempo real para gestão de estoque e coleta do pedido.",
     banner: "",
-    index : 5
+    index: 5,
   },
   {
     title: "S&OP",
     text: "Evite atrasos e reclamações, com Planejamento de Vendas e Operação é possível alinhar e integrar as áreas para melhor gerenciamento de produtos e demandas garantindo a eficiência do processo logístico.",
     banner: "",
-    index : 6
+    index: 6,
   },
 ];
 
@@ -53,8 +53,8 @@ function LogisticaComPerformance() {
       <section className={style.CarrouselProducts}>
         <div className={style.Container}>
           <div className={style.BoxCarrouselProducts}>
-            <div className={style.ContainerContent}>
-              <div className={style.ContainerTitle}>
+            <div className={style.Controller}>
+              <div className={style.ControllerContent}>
                 <div>
                   <h1> Logística com Performance </h1> e indicadores de sucesso
                 </div>
@@ -68,6 +68,7 @@ function LogisticaComPerformance() {
             </div>
           </div>
         </div>
+
         <CarrouselCards content={content} />
       </section>
     </>
