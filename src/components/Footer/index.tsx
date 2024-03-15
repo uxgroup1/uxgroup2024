fefe2bf938e5092874116a08cbb5cbfd612f40d4
Kauê Lima <kaue.slima000@gmail.com>
2024-03-15 16:57:18 -0300

    teste

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -99,7 +99,7 @@ export default function Footer() {
               })}
             </div>
           </div>
-          <hr className="my-6 border-white sm:mx-auto lg:my-8" />
+          <hr className="my-6 border-transparent relative w-full lg:border-white sm:mx-auto lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-white sm:text-center">
               © 2024{" "}
--- src/components/institucional/cases/index.tsx
+++ src/components/institucional/cases/index.tsx
@@ -8,23 +8,26 @@ export function Cases() {
     {
       title: "Lorem Ipsum",
       text: "lorem Ips laborum et sapien et dolor sit amet",
-      imageSrc: "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      imageSrc:
+        "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       author: "Ema Watson",
-      position: "Marketing Manager at Stech"
+      position: "Marketing Manager at Stech",
     },
     {
       title: "UX Group",
       text: "lorem Ips laborum et sapien et dolor sit amet",
-      imageSrc: "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      imageSrc:
+        "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       author: "John Doe",
-      position: "Product Designer at XYZ"
+      position: "Product Designer at XYZ",
     },
     {
       title: "UX Log",
       text: "lorem Ips laborum et sapien et dolor sit amet",
-      imageSrc: "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      imageSrc:
+        "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       author: "Jane Smith",
-      position: "UI/UX Designer at ABC"
+      position: "UI/UX Designer at ABC",
     },
   ];
 
@@ -54,29 +57,29 @@ export function Cases() {
 
   return (
     <section className="bg-white flex justify-center items-center w-full">
-      <div className="w-full flex justify-center items-center flex-col px-6 py-10 mx-auto">
+      <div className="w-full flex justify-center items-center flex-col px-4 mx-auto">
         <h1 className="mt-2 font-semibold text-black">
           Nossos cases de sucesso
         </h1>
 
-        <main className="relative flex justify-center items-center z-20 w-[92%] mt-8 md:flex md:items-center xl:mt-12">
+        <main className="relative flex justify-center items-center z-20 lg:w-[92%] mt-8 md:flex md:items-center xl:mt-12">
           <div className="absolute flex justify-center items-center  w-full bg-[#bfbfbf] -z-10 md:h-96 rounded-2xl"></div>
-
-          <div className="w-full p-6 bg-black md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
+          <div className="w-full bg-black md:flex md:items-center rounded-2xl md:bg-transparent md:p-0 lg:px-12 md:justify-evenly">
             <img
-              className={`h-40 w-40 md:mx-6 rounded-full object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[50%] md:rounded-2xl transition-opacity ${
+              className={`h-40 w-full md:mx-6 rounded-t-[16px] object-cover shadow-md md:h-[32rem] md:w-80 lg:h-[36rem] lg:w-[50%] md:rounded-2xl transition-opacity ${
                 isTransitioning ? "opacity-0" : "opacity-100"
               }`}
               src={slides[currentSlide].imageSrc}
               alt="client photo"
             />
-
-            <div className="mt-2 md:mx-6">
+            <div className="mt-2 md:mx-6 p-5">
               <div>
                 <p className="text-2xl font-medium tracking-tight text-white lg:text-black">
                   {slides[currentSlide].author}
                 </p>
-                <p className="text-white lg:text-black">{slides[currentSlide].position}</p>
+                <p className="text-white lg:text-black">
+                  {slides[currentSlide].position}
+                </p>
               </div>
 
               <p className="mt-4 text-lg leading-relaxed text-white lg:text-black md:text-xl transition-opacity">
