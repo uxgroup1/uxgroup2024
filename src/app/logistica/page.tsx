6aeac7a2d714d2ac63e5e82b946e6d5e33bf763d
Kauê Lima <kaue.slima000@gmail.com>
2024-03-28 17:53:32 -0300

    Cases

--- src/app/logistica/page.tsx
+++ src/app/logistica/page.tsx
@@ -10,7 +10,7 @@ const JumboTron = dynamic(() => import("@/components/produtos/jumbotron"));
 const SecondSection = dynamic(
   () => import("@/components/produtos/SecondSection")
 );
-import { Cases } from "@/components/institucional/cases";
+// import { Cases } from "@/components/institucional/cases";
 const CarroselImages = dynamic(
   () => import("@/components/produtos/carrouselVertical")
 );
@@ -26,15 +26,7 @@ import itajaiImage from "@/assets/logistica/cdsImages/itajai.jpg";
 import anvisaImage from "@/assets/logistica/anvisa.png";
 import fulfillmet from "@/assets/logos/fulfillment.svg"
 
-const slides = [
-  {
-    title: "Lorem Ipsum",
-    text: "lorem Ips laborum et sapien et dolor sit amet",
-    imageSrc:
-      "https://images.unsplash.com/photo-1488508872907-592763824245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    author: "Ema Watson",
-  },
-];
+
 
 const CarroselImagesContent = [
   {
@@ -101,7 +93,7 @@ export default function Logistica() {
         text="Nosso foco é fornecer soluções de fulfillment sob medida para atender às demandas específicas do seu negócio. Desde o armazenamento até a entrega, cuidamos de cada etapa do processo para garantir uma experiência de compra excepcional para seus clientes."
         image="."
       />
-      <Cases content={slides} />
+      {/* <Cases content={slides} /> */}
       <SecondSection
         corButtonOne="#fbc705"
         corButtonThree="#D3A707"
