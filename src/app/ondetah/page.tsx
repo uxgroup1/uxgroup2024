c856e78ca0b16e7ac4fd6d6dddab4d8ced16de05
Kauê Lima <kaue.slima000@gmail.com>
2024-04-08 15:32:17 -0300

    Tag

--- src/app/ondetah/page.tsx
+++ src/app/ondetah/page.tsx
@@ -30,6 +30,7 @@ import imageSlideTwo from "@/assets/ondetah/imagesSections/2.png";
 import imageSlideThree from "@/assets/ondetah/imagesSections/3.png";
 import imageSlideFour from "@/assets/ondetah/imagesSections/4.png";
 import Numbers from "@/components/produtos/numbers";
+import Script from "next/script";
 
 const contentFeatures = [
   {
@@ -137,6 +138,27 @@ const number = [
 export default function Ondetah() {
   return (
     <main>
+      {/* <!-- Google Tag Manager --> */}
+      <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-TKFK2WW');`}
+      </Script>
+      {/* <!-- End Google Tag Manager --> */}
+
+      {/* <!-- Google Tag Manager (noscript) --> */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TKFK2WW"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+      {/* <!-- End Google Tag Manager (noscript) --> */}
+
       <Header />
       <JumboTron
         textButtonOne="fale com um especialista"
