6ecb15eb242b2dd9e7825df633abd1f2e309214e
Kauê Lima <kaue.slima000@gmail.com>
2024-04-08 09:56:14 -0300

    Teste de responsividade

--- src/components/produtos/institucional/index.tsx
+++ src/components/produtos/institucional/index.tsx
@@ -48,36 +48,36 @@ export default function ProductInstitutional({
   }
 
   return (
-    <section className="bg-white max-w-[1996px] w-full lg:h-[80vh]">
+    <section className="bg-white max-w-[1996px] w-full xl:h-[80vh] lg:h-[80vh]">
       <div className="relative h-full flex">
         <div className="h-full lg:w-1/3"></div>
-        <div className="hidden w-3/4 rounded-2xl  bg-gray-100 h-full lg:block"></div>
+        <div className="hidden w-3/4 rounded-2xl  bg-gray-100 xl:h-full  lg:block"></div>
 
         <div className="w-[90%] flex items-center flex-col justify-center h-full lg:px-0  lg:py-10 mx-auto lg:absolute lg:inset-x-0">
           <div className="lg:flex lg:items-center">
             <Image
               src={banner}
-              className="w-full lg:w-[52rem] rounded-2xl shadow-2xl "
+              className="w-full xl:w-[52rem] lg:w-[30rem] rounded-2xl shadow-2xl "
               width={100}
               height={100}
               alt="ESG"
             />
 
             <div className="flex w-full lg:pl-24 lg:w-4/5 flex-col gap-5 mt-8 lg:px-10 lg:mt-0">
-              <h1 className="text-black lg:w-[90%] ">{title}</h1>
+              <h1 className="text-black xl:text-3xl lg:text-xl lg:w-[90%] ">{title}</h1>
 
-              <p className="lg:w-4/5 w-full text-black">{text}</p>
+              <p className="lg:w-4/5 w-full xl:text-sm lg:text-[0.7rem] text-black">{text}</p>
 
               <div className="flex lg:flex-row flex-col items-center gap-5">
                 <button
-                  className={`lg:w-[170px] w-full  h-10 rounded-lg text-white hover:font-semibold transition-all`}
+                  className={`xl:w-[170px] lg:w-[150px] xl:text-[0.9rem] lg:text-xs w-full  h-10 rounded-lg text-white hover:font-semibold transition-all`}
                   style={buttonStyle}
                   onMouseEnter={() => setHovered(true)}
                   onMouseLeave={() => setHovered(false)}
                 >
                   <Link href={linkButtonTwo}>{textButtonOne}</Link>
                 </button>
-                <button className="lg:w-[170px] border w-full  h-10 rounded-lg text-black hover:font-semibold transition-all">
+                <button className="xl:w-[170px] lg:w-[150px] xl:text-[0.9rem] lg:text-xs border w-full  h-10 rounded-lg text-black hover:font-semibold transition-all">
                   <Link href={linkButtonOne}> {textButtonTwo}</Link>
                 </button>
               </div>
