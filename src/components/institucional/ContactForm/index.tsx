b5eae4cce106b173bd4538f7db216716c597418a
Kauê Lima <kaue.slima000@gmail.com>
2024-04-10 16:10:40 -0300

    alterações dos produtos

--- src/components/institucional/ContactForm/index.tsx
+++ src/components/institucional/ContactForm/index.tsx
@@ -134,14 +134,14 @@ export default function ContactForm() {
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="text"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                name="floating_phone"
-                id="floating_phone"
+                name="floating_first_message"
+                id="floating_first_message"
                 className="block py-2.5 px-0 w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-white peer"
-                placeholder=""
+                placeholder=" "
+                required
               />
               <label
-                htmlFor="floating_phone"
+                htmlFor="floating_first_name"
                 className="peer-focus:font-medium absolute text-sm text-[#bfbfbf] duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-white  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
               >
                 Mensagem
