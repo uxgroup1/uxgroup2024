43b2f851ade348bd47bba2e6867a8db5e87a05f8
Kauê Lima <kaue.slima000@gmail.com>
2024-07-29 20:29:46 -0300

    Ajuste

--- src/components/TwoCardsHome/index.tsx
+++ src/components/TwoCardsHome/index.tsx
@@ -57,7 +57,7 @@ export default function NewTwoCards({ title, text, textFour, backgroundOne, back
 
 
                                 <p className=" lg:hidden text-base flex w-[200px] lg:w-auto gap-2 items-center left-[40%] justify-center  rounded-full px-4  text-black lg:left-[38%] p-2 bg-white ">
-                                    Powered
+                                    Powered by
                                     <Image className="w-16 mb-1" src={logoUnbox.src} width={100} height={100} alt="" />
                                 </p>
                             </div>
