324c09c9ca31f3977df41f772673b8fce8a919d9
Kauê Lima <kaue.slima000@gmail.com>
2024-07-30 09:55:41 -0300

    Ajuste

--- src/app/connect/page.tsx
+++ src/app/connect/page.tsx
@@ -29,9 +29,6 @@ export default function Connect() {
       value: "Quero fazer parte das conexões da UX",
     },
     {
-      value: "Fazer parte do ecossistema UX",
-    },
-    {
       value: "Quero saber mais",
     },
     {
