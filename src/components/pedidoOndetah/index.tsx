b1e5a14643f4a2c895b9682d6f2ee4a9edaf654d
Kauê Lima <kaue.slima000@gmail.com>
2024-08-26 11:21:37 -0300

    fix ajuste finalizado

--- src/components/pedidoOndetah/index.tsx
+++ src/components/pedidoOndetah/index.tsx
@@ -122,7 +122,7 @@ export default function PedidoOndetah() {
         onClick={() => handleOpenClick()}
         data-modal-target="static-modal"
         data-modal-toggle="static-modal"
-        className="block w-[95%] md:mt-0 mt-3  text-black   bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        className="block w-[98%] md:mt-0 mt-3  text-black   bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
         type="button"
       >
         Localizar meu pedido
