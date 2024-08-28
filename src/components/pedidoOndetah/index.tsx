a2e67f9f9bc827fe207eaecba0f5db33d636ceba
Kauê Lima <kaue.slima000@gmail.com>
2024-08-28 14:20:12 -0300

    Localizar pedido

--- src/components/pedidoOndetah/index.tsx
+++ src/components/pedidoOndetah/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoClose } from "react-icons/io5";
+import style from "./style.module.scss";
 import carrefour from "@/assets/logosOndetah/carrefour.png";
 import dafiti from "@/assets/logosOndetah/dafiti.png";
 import curta from "@/assets/logosOndetah/curta.png";
@@ -9,8 +10,13 @@ import consul from "@/assets/logosOndetah/consul.png";
 import brastemp from "@/assets/logosOndetah/brastemp.png";
 import compraCerta from "@/assets/logosOndetah/compra-certa.png";
 import kitchenaid from "@/assets/logosOndetah/kitchenaid.png";
+import whp from "@/assets/logosOndetah/whp.png";
+import icone from "@/assets/iconeOndetahLocalizarPedido.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { MdArrowLeft } from "react-icons/md";
+import { FaArrowLeft } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 export default function PedidoOndetah() {
   const [modalOpen, setModalOpen] = useState(false);
@@ -18,8 +24,10 @@ export default function PedidoOndetah() {
   const [codigoTransport, setCodigoTransport] = useState("");
   const [messageError, setMessageError] = useState("");
   const [linkUrl, setLinkUrl] = useState("");
+  const [primeiraEtapa, setPrimeiraEtapa] = useState(true);
+  const [segundaEtapa, setSegundaEtapa] = useState(false);
+  const [voltar, setVoltar] = useState(false);
 
-  console.log(codigoTransport);
 
 
   const [clicked, setClicked] = useState();
@@ -48,17 +56,28 @@ export default function PedidoOndetah() {
     setCodigoPedido(event.target.value);
   };
 
-  const handleCodigoTransport = (codigo: {codigo: string, index: any}) => {
+  const handleCodigoTransport = (codigo: { codigo: string, index: any }) => {
     setCodigoTransport(codigo.codigo);
     setClicked(codigo.index);
+    setSegundaEtapa(true);
+    setPrimeiraEtapa(false)
+    // primeiraEtapa ? setSegundaEtapa(false) : setSegundaEtapa(true);
   };
 
   const handleOpenClick = () => {
     setModalOpen(true);
   };
 
+  const handleVolta = () => {
+    setPrimeiraEtapa(true);
+    setSegundaEtapa(false);
+    setMessageError("")
+    setCodigoPedido("");
+  }
+
   const handleClose = () => {
     setModalOpen(false);
+    setMessageError("")
   };
 
   const content = [
@@ -89,6 +108,11 @@ export default function PedidoOndetah() {
       codigo: "DL",
     },
     {
+      index: 8,
+      image: whp.src,
+      codigo: "WHP",
+    },
+    {
       index: 5,
       image: consul.src,
       codigo: "WHP",
@@ -116,6 +140,16 @@ export default function PedidoOndetah() {
     // },
   ];
 
+  useEffect(() => {
+    segundaEtapa ? setVoltar(true) : setVoltar(false);
+
+    if(codigoPedido.length > 0){
+      setMessageError("");
+    } 
+  })
+
+
+
   return (
     <>
       <button
@@ -129,79 +163,109 @@ export default function PedidoOndetah() {
       </button>
 
       {modalOpen && (
-        <div
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 100 }}
+          transition={{ duration: 0.3 }}
           id="static-modal"
           data-modal-backdrop="static"
           aria-hidden="true" style={{ zIndex: 100 }}
           className={`fixed bg-[#00000093] transition-all flex items-center justify-center w-full left-0 h-[120vh]  top-0`}
         >
-          <div className="flex absolute md:top-1/4 top-10 items-center justify-center md:w-[700px]  w-[400px] bg-white rounded-2xl h-[700px] md:h-[650px] transition-all">
-            <div className="p-10 flex items-center w-full justify-center flex-col">
-              <div className="flex items-center justify-center w-full flex-col">
-                <div className="w-full pb-10 flex justify-end items-end">
-                  <IoClose
-                    onClick={() => handleClose()}
-                    className="text-black text-2xl hover:rotate-180 hover:text-[#ff0000] hover:bg-[#dedede] rounded-lg transition-all"
-                  />
-                </div>
-                <h1 className="md:text-4xl md:pl-0 pl-[10px] leading-normal md:text-left flex justify-center items-center w-full text-[19px] text-[#29235c]">
-                  Digite o código do seu pedido:
-                </h1>
+
+          <div className="flex absolute md:top-1/4 top-10 items-center justify-center md:w-[700px]  w-[400px] bg-white rounded-2xl h-[500px] md:h-[500px] transition-all">
+            <div className="p-10 h-full flex items-center w-full justify-center flex-col">
+              <div className={`w-full   flex ${voltar ? "justify-between" : "justify-end pt-[10px]"}  items-center`}>
+                {voltar && (
+                  <motion.div initial={{ opacity: 0 }}
+                    animate={{ opacity: 100 }}
+                    transition={{ duration: 0.4 }}
+                    onClick={() => handleVolta()} className="text-black hover:shadow-sm py-2 cursor-pointer flex gap-2 ml-3 items-center justify-center flex-row-reverse  text-lg px-2  opacity-95 rounded-lg ">voltar <FaArrowLeft /></motion.div>
+
+                )}
+                <IoClose
+                  onClick={() => handleClose()}
+                  className="text-black text-2xl hover:rotate-180 hover:text-[#ff0000] hover:bg-[#dedede] rounded-lg transition-all"
+                />
               </div>
 
-              <form className="w-full flex items-center justify-center pt-0 md:pt-4">
-                <div className="relative z-0 pt-5 w-[90%] mb-5 group">
-                  <input
-                    type="text"
-                    name="floating_first_name"
-                    id="floating_first_name"
-                    className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gradi appearance-none border-[#0095815b]  focus:outline-none focus:ring-0 focus:border-[#009580] peer"
-                    placeholder=""
-                    required
-                    value={codigoPedido}
-                    onChange={(event) => handleCodigoPedido(event)} // Passando o evento para a função handleCodigoPedido
-                  />
-                  <label
-                    htmlFor="floating_first_name"
-                    className="peer-focus:font-medium absolute text-sm text-[#009580] duration-300 transform -translate-y-2 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[#29265B]  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-2"
-                  >
-                    Código do Pedido:
-                  </label>
-                </div>
-              </form>
-              <div className="w-[90%] h-full">
-                <div className="w-full h-full flex flex-col items-center justify-center">
-                  <h3 className="text-[#29235c]">
-                    Selecione a loja onde realizou a compra:
-                  </h3>
-                  <div className="w-full md:flex md:items-center md:justify-center gap-3 grid md:pt-2 pt-3 grid-cols-4 md:flex-wrap">
-                    {content.map((content, index) => {
-                      return (
-                        <div
-                          onClick={() => handleCodigoTransport(content)}
-                          key={index}
-                          className={`hover:shadow-md hover:scale-90 transition border-[1px] border-[#0000000f]  rounded-lg ${clicked === content.index ? "border-1 shadow-xl rounded-md" : ""}  md:w-[100px] w-[70px]`}
-                        >
-                          <Image
-                            className={` rounded-lg ${clicked === content.codigo ? "border-1 shadow-xl p-1 rounded-md" : ""} focus:p-4 transition-all`}
-                            src={content.image}
-                            quality={100}
-                            priority
-                            width={100}
-                            height={100}
-                            alt="Clientes ondetah"
-                          />
-                        </div>
-                      );
-                    })}
+
+              {primeiraEtapa && (
+                <motion.div initial={{ opacity: 0, translateX: -80 }}
+                  animate={{ translateX: 0, opacity: 100 }}
+                  transition={{ duration: 0.4 }} className=" flex items-center justify-center w-full h-full">
+                  <div className="w-full h-full gap-10 flex flex-col items-center justify-center">
+                    <div className="flex w-full pl-[15px]  items-center justify-start">
+                      <Image className="w-16" src={icone} alt="" width={100} height={100} />
+                      <h3 className="md:text-[25px] md:pl-0 pl-[10px] leading-normal md:text-center flex justify-center items-center w-full text-[19px] text-[#29235c]">
+                        Selecione a loja onde realizou a compra:
+                      </h3>
+                    </div>
+
+                    <div className="w-full md:flex md:items-center md:justify-center gap-3 grid md:pt-2 pt-3 grid-cols-4 md:flex-wrap">
+                      {content.map((content, index) => {
+                        return (
+                          <div
+                            onClick={() => handleCodigoTransport(content)}
+                            key={index}
+                            className={`hover:shadow-md shadow-lg  hover:scale-90 transition border-[1px] border-[#0000000f]  rounded-lg ${clicked === content.index ? "border-1 shadow-xl rounded-md" : ""}  md:w-[110px] w-[70px]`}
+                          >
+                            <Image
+                              className={` rounded-lg ${clicked === content.codigo ? "border-1 shadow-xl p-1 rounded-md" : ""} focus:p-4 transition-all`}
+                              src={content.image}
+                              quality={100}
+                              priority
+                              width={100}
+                              height={100}
+                              alt="Clientes ondetah"
+                            />
+                          </div>
+                        );
+                      })}
+                    </div>
                   </div>
+                </motion.div>
+              )}
+
+              {segundaEtapa && (
+                <motion.div
+                  initial={{ opacity: 0, translateX: 100 }}
+                  animate={{ translateX: 0, opacity: 100 }}
+                  transition={{ duration: 0.4 }}
+                  className=" flex flex-col items-center justify-center w-full h-full">
+                  <div className="flex items-center justify-center w-full flex-col">
+                    <div className="flex w-[95%] items-center justify-start">
+                      <Image className="w-16" src={icone} alt="" width={100} height={100} />
+                      <h3 className="md:text-[28px] md:pl-4  pl-[10px] leading-normal md:text-left flex justify-start items-center w-full  text-[#29235c]">
+                        Digite o código do seu pedido:
+                      </h3>
+                    </div>
+                  </div>
+
+                  <form className="w-full gap-2 flex items-center justify-center pt-0 md:pt-4">
+                    {/* <Image className="w-10" src={icone} alt="" width={100} height={100} /> */}
+                    <div className="relative z-0 pt-5 w-[95%] mb-5 group">
+                      <input
+                        type="text"
+                        name="floating_first_name"
+                        id="floating_first_name"
+                        className="block py-2.5 px-0 w-full text-sm text-black bg-transparent border-0 border-b-2 border-gradi appearance-none border-[#0095815b]  focus:outline-none focus:ring-0 focus:border-[#009580] peer"
+                        placeholder="Digite o código do seu pedido aqui:"
+                        required
+                        value={codigoPedido}
+                        onChange={(event) => handleCodigoPedido(event)} // Passando o evento para a função handleCodigoPedido
+                      />
+
+                    </div>
+                  </form>
+
                   {messageError && (
                     <p className="text-red-500 md:text-lg text-sm pb-0 pt-4">
                       {messageError}
                     </p>
                   )}
                   <Link
-                    className="w-full h-14 mt-5 hover:p-[1px] transition-all rounded-2xl"
+                    className="w-[95%] h-14 mt-5 hover:p-[1px] transition-all rounded-2xl"
                     onClick={(e) => {
                       const linkResult = handleSetLink(
                         codigoTransport,
@@ -219,16 +283,17 @@ export default function PedidoOndetah() {
                   >
                     <button
                       type="submit"
-                      className="w-full h-full hover:shadow-xl transition-all text-white rounded-lg bg-[#009580]"
+                      className={style.unique}
                     >
                       localizar pedido
                     </button>
                   </Link>
-                </div>
-              </div>
+
+                </motion.div>
+              )}
             </div>
           </div>
-        </div>
+        </motion.div >
       )}
     </>
   );
