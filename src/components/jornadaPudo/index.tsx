a6c5585257dbf2a6a336ec01aa6602c02c75f6b6
Kauê Lima <kaue.slima000@gmail.com>
2025-07-22 09:55:17 -0300

    Formulario mobile

--- src/components/jornadaPudo/index.tsx
+++ src/components/jornadaPudo/index.tsx
@@ -20,7 +20,7 @@ export default function JornadaPudo() {
 
 
     const viewFormularioQueroSerUmPudo = () => {
-        
+
         setFormularioQueroSerPudo(true);
         console.log(formularioQueroSerPudo);
 
@@ -89,10 +89,10 @@ export default function JornadaPudo() {
 
 
     return (
-        <section id="quero-ser-um-pudo" className="w-full bg-cover flex items-center justify-center bg-center bg-no-repeat bg-white h-auto pt-[70px] pb-[100px] ">
+        <section id="quero-ser-um-pudo" className="w-full bg-cover  flex items-center justify-center bg-center bg-no-repeat bg-white h-auto pt-[70px] pb-[100px] ">
             <div className="w-[90%] lg:w-full max-w-[90%] gap-5 lg:gap-10 h-auto flex lg:flex-row flex-col-reverse items-center justify-between">
-                <div className="w-full flex flex-col h-auto gap-3 overflow-auto">
-                    <div className="w-full mb-20 lg:hidden flex items-start gap-3 flex-col h-auto">
+                <div className="w-full flex flex-col h-auto gap-3 overflow-hidden">
+                    <div className="w-full  mb-20 lg:hidden flex items-start gap-3 flex-col h-auto">
                         <Image src={boxPudo} width={100} height={100} alt="Box Orange" />
                         <h1 className="text-black">
                             Transforme a jornada do cliente com PUDO.
@@ -103,74 +103,76 @@ export default function JornadaPudo() {
                         <button onClick={() => viewFormularioQueroSerUmPudo()} className="h-12 w-full text-white bg-[#FF7F13] rounded-lg shadow">quero ser um PUDO</button>
                     </div>
                     {formularioQueroSerPudo && (
-                                <FormularioQueroSerUmPudo onClose={() => setFormularioQueroSerPudo(false)} />
-                            )}
-                    <div className="lg:w-full w-[875px] justify-end gap-5 mb-5 h-auto flex">
-                        <div className="h-20 lg:w-1/4 lg:flex hidden w-[300px] rounded-lg bg-[#f3f4f600]">
+                        <FormularioQueroSerUmPudo onClose={() => setFormularioQueroSerPudo(false)} />
+                    )}
+                    <div className="w-full overflow-auto">
+                        <div className="lg:w-full w-[875px] justify-end gap-5 mb-5 h-auto flex">
+                            <div className="h-20 lg:w-1/4 lg:flex hidden w-[300px] rounded-lg bg-[#f3f4f600]">
 
-                        </div>
-                        <div className="h-20 lg:w-1/4 lg:flex  w-[170px] rounded-lg bg-[#f3f4f600]">
+                            </div>
+                            <div className="h-20 lg:w-1/4 lg:flex  w-[170px] rounded-lg bg-[#f3f4f600]">
 
+                            </div>
+                            <div className="h-22 p-5 lg:w-1/4 w-[350px] flex items-center text-black justify-center text-lg font-bold rounded-lg shadow bg-[#F3F4F6]">
+                                Sem PUDO
+                            </div>
+                            <div className="h-22 p-5 flex items-center justify-center lg:w-1/4 w-[350px] text-white gap-5 text-lg font-bold rounded-lg shadow bg-[#FF7F13]">
+                                <Image className="w-[50px]" src={iconPudo} alt="" width={100} height={100} />
+                                Com PUDO
+                            </div>
                         </div>
-                        <div className="h-22 p-5 lg:w-1/4 w-[350px] flex items-center text-black justify-center text-lg font-bold rounded-lg shadow bg-[#F3F4F6]">
-                            Sem PUDO
-                        </div>
-                        <div className="h-22 p-5 flex items-center justify-center lg:w-1/4 w-[350px] text-white gap-5 text-lg font-bold rounded-lg shadow bg-[#FF7F13]">
-                            <Image className="w-[50px]" src={iconPudo} alt="" width={100} height={100} />
-                            Com PUDO
-                        </div>
-                    </div>
-                    <div className="lg:w-full w-[800px] flex gap-5 h-auto overflow-auto">
-                        <div className="w-1/4 hidden lg:flex items-start gap-5 flex-col h-auto">
-                            <Image src={boxPudo} width={100} height={100} alt="Box Orange" />
-                            <h1 className="text-black">
-                                Transforme a jornada do cliente com PUDO.
-                            </h1>
-                            <p className="text-black font-light">
-                                Compare os benefícios de empresas que adotam o PUDO em relação às que ainda operam sem esse modelo e descubra como essa solução pode transformar sua logística.
-                            </p>
-                            <button onClick={() => viewFormularioQueroSerUmPudo()} className="h-12 w-[300px] text-white bg-[#FF7F13] rounded-lg shadow">quero ser um PUDO</button>
+                        <div className="lg:w-full w-[800px] flex gap-5 h-auto overflow-auto">
+                            <div className="w-1/4 hidden lg:flex items-start gap-5 flex-col h-auto">
+                                <Image src={boxPudo} width={100} height={100} alt="Box Orange" />
+                                <h1 className="text-black">
+                                    Transforme a jornada do cliente com PUDO.
+                                </h1>
+                                <p className="text-black font-light">
+                                    Compare os benefícios de empresas que adotam o PUDO em relação às que ainda operam sem esse modelo e descubra como essa solução pode transformar sua logística.
+                                </p>
+                                <button onClick={() => viewFormularioQueroSerUmPudo()} className="h-12 w-[300px] text-white bg-[#FF7F13] rounded-lg shadow">quero ser um PUDO</button>
 
-                            {formularioQueroSerPudo && (
-                                <FormularioQueroSerUmPudo onClose={() => setFormularioQueroSerPudo(false)} />
-                            )}
-                        </div>
-                        <div className="lg:w-1/4 w-[200px] flex gap-5 flex-col h-auto">
-                            {titulos.map((t => (
-                                <div className="w-full lg:w-[235px] lg:my-0 my-5  text-lg flex items-center p-5 h-[180px] text-[#FF7F13] font-bold border-l border-l-[#FF7F13]">
-                                    {t.title}
-                                </div>
-                            )))}
-                        </div>
-                        <div className="lg:w-1/4 w-[400px] flex gap-5  flex-col h-auto">
-                            {semPudo.map((s => (
-                                <div className="w-full text-lg flex-col lg:my-0 my-5 flex justify-center items-center p-5 h-[180px] text-black font-bold border-l border-l-[#8D8D8D]">
-                                    <div className="w-full flex gap-3 flex-col">
-                                        <div className="w-10 h-10 rounded-lg border-[#8D8D8D] border flex items-center justify-center text-[#8D8D8D] bg-[#F3F4F6]">
-                                            <s.icon fontWeight={700} fontSize={25} />
-                                        </div>
-                                        <h2 className="text-lg font-semibold">
-                                            {s.title},<p className="font-light">{s.text}</p>
-                                        </h2>
+                                {formularioQueroSerPudo && (
+                                    <FormularioQueroSerUmPudo onClose={() => setFormularioQueroSerPudo(false)} />
+                                )}
+                            </div>
+                            <div className="lg:w-1/4 w-[200px] flex gap-5 flex-col h-auto">
+                                {titulos.map((t => (
+                                    <div className="w-full lg:w-[235px] lg:my-0 my-5  text-lg flex items-center p-5 h-[180px] text-[#FF7F13] font-bold border-l border-l-[#FF7F13]">
+                                        {t.title}
                                     </div>
-
-                                </div>
-                            )))}
-                        </div>
-                        <div className="lg:w-1/4 w-[400px] flex gap-5  flex-col h-auto">
-                            {comPudo.map((s => (
-                                <div className="w-full text-lg flex-col lg:my-0 my-5 flex justify-center items-center p-5 h-[180px] text-black font-bold border-l border-l-[#8D8D8D]">
-                                    <div className="w-full flex gap-3 flex-col">
-                                        <div className="w-10 h-10 rounded-lg border-[#FF7F13] border flex items-center justify-center text-[#FF7F13] bg-[#FF7F1333]">
-                                            <s.icon fontWeight={700} fontSize={25} />
+                                )))}
+                            </div>
+                            <div className="lg:w-1/4 w-[400px] lg:mr-0 mr-12 flex gap-5  flex-col h-auto">
+                                {semPudo.map((s => (
+                                    <div className="w-full text-lg flex-col lg:my-0 my-5 flex justify-center items-center p-5 h-[180px] text-black font-bold border-l border-l-[#8D8D8D]">
+                                        <div className="w-full flex gap-3 flex-col">
+                                            <div className="w-10 h-10 rounded-lg border-[#8D8D8D] border flex items-center justify-center text-[#8D8D8D] bg-[#F3F4F6]">
+                                                <s.icon fontWeight={700} fontSize={25} />
+                                            </div>
+                                            <h2 className="text-lg font-semibold">
+                                                {s.title},<p className="font-light">{s.text}</p>
+                                            </h2>
                                         </div>
-                                        <h2 className="text-lg font-semibold">
-                                            {s.title},<p className="font-light">{s.text}</p>
-                                        </h2>
-                                    </div>
 
-                                </div>
-                            )))}
+                                    </div>
+                                )))}
+                            </div>
+                            <div className="lg:w-1/4 w-[400px] flex gap-5  flex-col h-auto">
+                                {comPudo.map((s => (
+                                    <div className="w-full text-lg flex-col lg:my-0 my-5 flex justify-center items-center p-5 h-[180px] text-black font-bold border-l border-l-[#8D8D8D]">
+                                        <div className="w-full flex gap-3 flex-col">
+                                            <div className="w-10 h-10 rounded-lg border-[#FF7F13] border flex items-center justify-center text-[#FF7F13] bg-[#FF7F1333]">
+                                                <s.icon fontWeight={700} fontSize={25} />
+                                            </div>
+                                            <h2 className="text-lg font-semibold">
+                                                {s.title},<p className="font-light">{s.text}</p>
+                                            </h2>
+                                        </div>
+
+                                    </div>
+                                )))}
+                            </div>
                         </div>
                     </div>
                 </div>
